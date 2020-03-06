3a173ed64b001151e9b9d3e93327198fb3b32f87
Mark Stabler <mark@stablerwritingservices.com>
2020-03-06 20:29:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -7,7 +7,7 @@ def summon_captain_planet(calls_list)
 end
 
 def long_planeteer_calls(calls_list)
-	calls_list.any? { |call| call.length > 6 }
+	calls_list.any? { |call| call.length > 5 }
 end
 
 def find_the_cheese# code an argument here
